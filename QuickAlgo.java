4dff3a3b9a68dbfaa13ae2fb2acc4295c6ce4271
Unknown <marc-antoine.therrien@polymtl.ca>
2019-01-27 17:02:54 -0500

    tous les quicksort

--- QuickAlgo.java
+++ QuickAlgo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
-
-//source : http://www.java2novice.com/java-sorting-algorithms/quick-sort/
+//source : https://gist.github.com/aaani/6337280
 public class QuickAlgo implements AlgoInterface {
 
     private int pivot;
@@ -12,58 +12,78 @@ public class QuickAlgo implements AlgoInterface {
         this. threshold = threshold;
     }
 
-    private ArrayList<Integer> tempArray = new ArrayList<Integer>();
-    private int length;
-
 	public ArrayList<Integer> handle(ArrayList<Integer> numbers) {
-		
-        if (numbers == null || numbers.size() == 0) {
-            return new ArrayList<Integer>();
-        }
-        tempArray = numbers;
-        length = numbers.size();
-        quickSort(0, numbers.size() - 1);
-
-		return numbers;
+        quicksort(numbers,0, numbers.size()-1);
+        return numbers;
 	}
 
-    private void quickSort(int lowerIndex, int higherIndex) {
-         
-        int i = lowerIndex;
-        int j = higherIndex;
-        // calculate pivot number, I am taking pivot as middle index number
-        int pivot = tempArray.get(lowerIndex+(higherIndex-lowerIndex)/2);
-        // Divide into two arrays
-        while (i <= j) {
-            /**
-             * In each iteration, we will identify a number from left side which 
-             * is greater then the pivot value, and also we will identify a number 
-             * from right side which is less then the pivot value. Once the search 
-             * is done, then we exchange both numbers.
-             */
-            while (tempArray.get(i) < pivot) {
-                i++;
-            }
-            while (tempArray.get(j) > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                exchangeNumbers(i, j);
-                //move index to next position on both sides
-                i++;
-                j--;
+    public void swap(ArrayList<Integer> elements, int i, int j){
+        //Method to swap 2 elements in an arraylist
+        int temp= elements.get(i);
+        elements.set(i, elements.get(j));
+        elements.set(j, temp);
+    }
+    
+    public int partition(ArrayList<Integer> elements, int beg, int end){
+        
+        //Get a fonctionnal pivot
+        int myPivot;
+        if(pivot == 0){
+            myPivot = beg;
+        }
+        else{
+            myPivot = beg + ((int)Math.random()*(elements.size()))/(end-beg+1);
+        }
+
+        //New position of pivot element
+        int last = end;
+        
+        //Move the pivot element to right edge of the array
+        swap(elements, myPivot, end);
+        end--;
+        
+        while(beg <= end){
+            if(elements.get(beg) < elements.get(last)) beg++; //Accumulate smaller elements to the left
+            else {
+                swap(elements, beg, end);
+                end--;
             }
         }
-        // call quickSort() method recursively
-        if (lowerIndex < j)
-            quickSort(lowerIndex, j);
-        if (i < higherIndex)
-            quickSort(i, higherIndex);
+        
+        //Move pivot element to its correct position
+        swap(elements, beg, last);
+        
+        return beg;
     }
- 
-    private void exchangeNumbers(int i, int j) {
-        int temp = tempArray.get(i);
-        tempArray.set(i, tempArray.get(j));
-        tempArray.set(j, temp);
+    
+    public void quicksort(ArrayList<Integer> elements, int beg, int end){
+        if(beg >= end || beg < 0 || end > elements.size() - 1) return;
+        
+        int oldPivot = partition(elements, beg, end);
+        if(oldPivot - beg + 1 >= threshold){
+            quicksort(elements, beg, oldPivot-1);
+        }
+        else if(oldPivot - beg > 1){
+            replacteElem(elements, beg, oldPivot-1);
+        }
+        if(end - oldPivot + 1 >= threshold){
+            quicksort(elements, oldPivot+1, end);
+        }
+        else if(end - oldPivot > 1){
+            replacteElem(elements, oldPivot+1, end);
+        }
+        //quicksort(elements, beg, oldPivot-1);
+        //quicksort(elements, oldPivot+1, end);
     }
+
+    public void replacteElem(ArrayList<Integer> elements, int beg, int end){
+        ArrayList<Integer> tempList = new ArrayList<Integer>(elements.subList(beg, end));
+        Collections.sort(tempList);
+        int counter = 0;
+        for(int i = beg; i < end; i++){
+            elements.set(i, tempList.get(counter));
+            counter++;
+        }
+    }
+
 }
